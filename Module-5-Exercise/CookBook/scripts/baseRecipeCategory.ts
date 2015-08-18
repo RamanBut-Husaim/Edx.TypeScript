559453df6e6039c1e6d927881c2d1a7b186d9b32
RamanBut-Husaim <raman_but-husaim@live.com>
2015-08-19 01:25:47 +0300

    Initial commit.

---
+++ Module-5-Exercise/CookBook/scripts/baseRecipeCategory.ts
@@ -0,0 +1,9 @@
+﻿class BaseRecipeCategory {
+    private _name: string;
+    private _foodGroups: FoodGroup[] = [];
+
+    //TODO: Create get and set blocks for each of the variables in the declaration above.
+
+
+
+} 
---
+++ Module-5-Exercise/CookBook/scripts/foodGroup.ts
@@ -0,0 +1,7 @@
+﻿class FoodGroup {
+    private _name: string;
+
+    //TODO: Create a get and set block for the member variable above. 
+
+
+} 
