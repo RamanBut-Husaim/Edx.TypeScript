823c8c6582fe71ae72c358732c29ecc83e884bfb
RamanBut-Husaim <raman_but-husaim@live.com>
2015-08-21 01:04:28 +0300

    Remove ToDo note.

--- Module-5-Exercise/CookBook/scripts/baseRecipeCategory.ts
+++ Module-5-Exercise/CookBook/scripts/baseRecipeCategory.ts
@@ -2,8 +2,6 @@
     private _name: string;
     private _foodGroups: FoodGroup[] = [];
 
-    //TODO: Create get and set blocks for each of the variables in the declaration above.
-
     get name(): string {
         return this._name;
     };
